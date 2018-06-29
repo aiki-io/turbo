0dc8608e4ff79559f2a20ad0a463e491180d9393
Lech Gudalewicz <lech@aiki.io>
2018-06-29 16:40:46 -0700

    layout as javascript

--- spa/js/spa.js
+++ spa/js/spa.js
@@ -1,9 +1,21 @@
 let spa = () => {
-    let init = (container) => {
-        container.html(
-            '<h1 style="display: inline-block; margin: 25px;">hello, world</h1>'
 
-        );
+    let main_html =
+    '<div id="spa-shell-head">' +
+     '<div class="spa-shell-head-logo"></div>' +
+     '<div class="spa-shell-head-acct"></div>'+
+     '<div class="spa-shell-head-search"></div>' +
+     '</div>' +
+     '<div class="spa-shell-main">' +
+         '<div class="spa-shell-main-nav"></div>' +
+         '<div class="spa-shell-main-content"></div>' +
+     '</div>' +
+     '<div class="spa-shell-foot"></div>' +
+     '<div class="spa-shell-chat"></div>' +
+     '<div class="spa-shell-modal"></div>';
+
+    let init = (container) => {
+        container.html(main_html);
     };
 
     return { init };
